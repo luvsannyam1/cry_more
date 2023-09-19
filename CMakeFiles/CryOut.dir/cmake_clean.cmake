82a60cdc7abd6c4fe44c964e187a39ce75a4eb6e
Luvsannyam Bayarsaikhan <b.luvsannyam1@gmail.com>
2023-09-19 14:43:57 +0800

    FIRST IMPL

---
+++ CMakeFiles/CryOut.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CryOut.dir/src/main.cpp.o"
+  "CMakeFiles/CryOut.dir/src/main.cpp.o.d"
+  "bin/CryOut"
+  "bin/CryOut.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CryOut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/CryOut.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CryOut.
+# This may be replaced when dependencies are built.
